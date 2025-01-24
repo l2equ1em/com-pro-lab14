e32b6902a7a01c7f8abdf3816f42a6f870547f75
LAB303-24 <lab303-24@vdieng.local>
2025-01-24 16:00:39 +0700

    lab14_2.cpp completed

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,29 @@ int main(){
 }
 
 //Do not modify source code above this line 
+void showMatrix(const bool metrix[][N]){
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            cout << metrix[i][j] << " ";
+        }cout << endl;
+    }
+}
+void inputMatrix(double x[][N]){
+    for(int i = 0; i < N; i++){
+        cout << "Row " << i+1 << ": ";
+        for(int j = 0; j < N; j++) cin >> x[i][j];
+    }
+}
+void findLocalMax(const double input[][N], bool output[][N]){
+    for(int i =0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            if(i == 0 || j == 0 || i == N-1 || j == N-1){
+                output[i][j] = 0;
+            }
+            else if(input[i][j] >= input [i-1][j] && input[i][j] >= input[i+1][j] && input [i][j] >= input [i][j+1]  && input[i][j] >= input[i][j-1]){
+                output[i][j] = 1;
+            }
+            else output[i][j] = 0;
+        }
+    }
+}
